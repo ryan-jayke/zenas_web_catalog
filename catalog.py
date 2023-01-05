0b88f6e2009a8493b902b8d61e59319628d71198
Jay <102709775+ryan-jayke@users.noreply.github.com>
2023-01-05 11:51:14 -0500

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -2,6 +2,7 @@ import snowflake.connector
 import streamlit
 import pandas
 
-catalog_list = pandas.read_sql_query("select * from zenas_athleisure_db.products.catalog_for_website")
+sql_query = "select * from zenas_athleisure_db.products.catalog_for_website"
+
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
