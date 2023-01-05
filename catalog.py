c2e92ef3df65b417d63c1a6f94081d32e141e1ac
Jay <102709775+ryan-jayke@users.noreply.github.com>
2023-01-05 11:42:52 -0500

    testing direct query to snowflake view

--- catalog.py
+++ catalog.py
@@ -1,4 +1,7 @@
 import snowflake.connector
 import streamlit
+import pandas
+
+catalog_list = pandas.read_sql_query("select * from zenas_athleisure_db.products.catalog_for_website")
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
